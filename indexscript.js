f14b1958547ee627fd7937754e4ccfb1709a3852
JacoboSternberg <jacobo21blue@gmail.com>
2019-09-11 13:24:01 -1000

    remove float

--- indexscript.js
+++ indexscript.js
@@ -1,11 +1,11 @@
 var main = function() {
-    $('.wrapper').mouseenter(function() {
-	$('.LargeText').animate({'font-size': '55px'}, 500)
-    });
+    // $('.wrapper').mouseenter(function() {
+    // 	$('.LargeText').animate({'font-size': '55px'}, 500)
+    // });
 
-    $('.wrapper').mouseleave(function() {
-	$('.LargeText').animate({'font-size': '50px'}, 500)
-    });
+    // $('.wrapper').mouseleave(function() {
+    // 	$('.LargeText').animate({'font-size': '50px'}, 500)
+    // });
 };
 
 $(document).ready(main);
